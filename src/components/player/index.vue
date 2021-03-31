e24398bce2e170ebfa7e38951b2a2fb651554447
guoqingzeng <806340192@qq.com>
2021-03-31 15:59:24 +0800

    播放器

--- src/components/player/index.vue
+++ src/components/player/index.vue
@@ -136,10 +136,11 @@ export default {
 }
 
 .play-container {
-  width: 100%;
-  height: 100%;
-  top: 0;
   position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   z-index: 99;
   background: #fff;
 }
