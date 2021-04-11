ceb3733be5557f099a4816a934914ae64a8dca73
guoqingzeng <806340192@qq.com>
2021-04-11 15:21:33 +0800

    新增歌词api

--- src/api/player.js
+++ src/api/player.js
@@ -1,4 +1,15 @@
+import request from '../utils/request'
+
 // 拼接音乐url
-export function getSongUrl(id){
+export function getSongUrl(id) {
   return `https://music.163.com/song/media/outer/url?id=${id}.mp3 `
+}
+
+// 获取歌词
+export function getLyric(params) {
+  return request({
+    url: '/lyric',
+    method: 'get',
+    params
+  })
 }
