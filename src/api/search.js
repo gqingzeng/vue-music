e8ef578cac5e464b63a124fbde0706f8b8305a78
guoqingzeng <806340192@qq.com>
2021-04-18 12:53:59 +0800

    新增搜索api

--- src/api/search.js
+++ src/api/search.js
@@ -1,9 +1,19 @@
 import request from '../utils/request'
 
-// 热门搜索
-export function getHotSearch(){
+// 搜索
+export function searchQuery(params) {
   return request({
-    url:'/search/hot',
-    method:'get',
+    url: "/search",
+    params,
+    methods: 'get'
+  })
+}
+
+
+// 热门搜索
+export function getHotSearch() {
+  return request({
+    url: '/search/hot',
+    method: 'get',
   })
 }
