a2087c5f653bce85e5108b89b3fac49a5788c39a
guoqingzeng <806340192@qq.com>
2021-04-18 12:57:59 +0800

    新增暂无数据组件

---
+++ src/components/NoResult/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <i class="iconfont icon-zanwushuju"></i>
+    <p class="title">暂无数据</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+   return {
+
+   }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.container{
+  text-align: center;
+    color: rgba(255, 255, 255, 0.5);
+  .iconfont{
+    font-size: 120px;
+  }
+  .title{
+    margin-top: -30px;
+  }
+}
+</style>
